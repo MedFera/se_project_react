5d549a65d6a4592663d9949d41e2fb40b55c7983
Medin Feratovic <medferatovic.com>
2026-02-05 19:33:14 -0500

    Fixed Main.jsx issue with imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../App/App.jsx'
+import App from './components/App/App.jsx'
 import './index.css'
-import '/src/assets/fonts/CabinetGrotesk-Bold.otf'
-import '/src/assets/fonts/CabinetGrotesk-Medium.otf'
+import './assets/fonts/CabinetGrotesk-Bold.otf'
+import './assets/fonts/CabinetGrotesk-Medium.otf'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
